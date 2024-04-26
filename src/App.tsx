826f1e1fab98c24e7d9b9c17469cd9b38698bc2c
Jeevan Karnati <38685638+jeevanreddy1999@users.noreply.github.com>
2024-04-26 13:18:31 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <div className="loader">
+        <div className="loader__bar"></div>
+        <div className="loader__bar"></div>
+        <div className="loader__bar"></div>
+        <div className="loader__bar"></div>
+        <div className="loader__bar"></div>
+        <div className="loader__ball"></div>
+      </div>
+    </div>
+  );
+}
